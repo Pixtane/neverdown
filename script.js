413db1085eb3451cfefa092482c3a3afad952280
Pixtane <94226146+Pixtane@users.noreply.github.com>
2023-12-07 14:53:20 +0200

    neverdown

---
+++ script.js
@@ -0,0 +1,20 @@
+// script.js
+// Changes color of background and redirects to next page
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Your existing script
+
+  // Add functionality to change background color
+  const changeColorBtn = document.getElementById("myBTN");
+
+  changeColorBtn.addEventListener("click", function () {
+    // Generate a random hex color
+    const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    // Change the background color
+    document.body.style.backgroundColor = randomColor;
+  });
+  setTimeout(function () {
+    window.location.href = "https://rb.gy/7f1269";
+  }, 1000);
+});
